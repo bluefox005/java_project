6be8e07d4731000f8fb5efbee815833a37fa0b9a
William <bluefox005.ca@gmail.com>
2018-03-03 22:26:19 -0500

    new items

---
+++ src/test/java/com/ecvictor/selenium/junit/page_object_iga/module/HomePageAction.java
@@ -0,0 +1,22 @@
+package com.ecvictor.selenium.junit.page_object_iga.module;
+
+import com.ecvictor.selenium.junit.page_object_iga.page_object.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+ public  class  HomePageAction extends BaseAction {
+
+    public static void clickMyAccount() {
+        driver.findElement(By.xpath(HomePage.myAccount)).click();
+        assertEquals(driver.findElement(By.xpath("//H1[@class='push-medium--bottom']")).getText(), "LOGIN");
+    }
+
+    public static void checkPromotionChicken() {
+        driver.findElement(By.xpath(HomePage.promotionChicken)).click();
+        assertEquals(driver.findElement(
+                By.cssSelector("h1.h3-like.product-detail__name > span")).getText(),
+                "Chicken Drumstick");
+    }
+}
---
+++ src/test/java/com/ecvictor/selenium/junit/page_object_iga/page_object/HomePage.java
@@ -0,0 +1,6 @@
+package com.ecvictor.selenium.junit.page_object_iga.page_object;
+
+public class HomePage {
+    public final static String myAccount = "//a[@id='header_0_MobileAccountLink']";
+    public final static String promotionChicken = "//a[@title=\"Fresh Chicken Drumstick\"]";
+}
